ffa9f86ca5030fe21e3746e62fc8d4fe98033fd7
Ivan Trepashka <itrepashka@fxcess.com>
2026-03-02 22:55:32 +0700

    add CorrelationIdMiddleware

---
+++ src/shared/csharp/Shared/Shared.Web/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+using Shared.Web.Middlewares;
+
+namespace Shared.Web.Extensions;
+
+public static class ApplicationBuilderExtensions 
+{
+    public static IApplicationBuilder AddCorrelationId(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<CorrelationIdMiddleware>();
+        return app;
+    }
+}
---
+++ src/shared/csharp/Shared/Shared.Web/Middlewares/CorrelationIdMiddleware.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Serilog.Context;
+
+namespace Shared.Web.Middlewares;
+
+public class CorrelationIdMiddleware : IMiddleware
+{
+    private const string CorrelationIdHeaderName = "X-Correlation-ID";
+    private const string CorrelationId = "CorrelationId";
+    
+    public Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        if (!context.Request.Headers.TryGetValue(CorrelationIdHeaderName, out var value))
+        {
+            value = Guid.NewGuid().ToString();
+        }
+        
+        context.Items[CorrelationId] =  value;
+        LogContext.PushProperty(CorrelationId, value);
+        context.Response.Headers[CorrelationIdHeaderName] = value;
+        
+        return next(context);
+    }
+}
